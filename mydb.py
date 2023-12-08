ade2d49d6cff57d029ef977b938a53f887aae64f
Johan Gabrielson <johan.gabrielson@gmail.com>
2023-12-08 20:22:22 +0100

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,16 @@
+import mysql.connector 
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'MuMu321'
+)
+
+# cursor object 
+cursorObject = dataBase.cursor()
+
+# create a database
+
+cursorObject.execute("CREATE DATABASE db1")
+
+print("Database created")
